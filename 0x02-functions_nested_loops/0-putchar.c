c99ee225b524962269be010e5ec43ce7377606da
Kachapin <kevinkachapin@gmail.com>
2022-09-15 01:00:53 +0300

    Printing with Single lines

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,13 @@
-#include"main.h"
+#include <unistd.h>
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: prints _putchar using putchar prototype
- *
+ * main - This program prints string on the
+ * display
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
-	_putchar('\n');
-
+	write(1, "_putchar\n", 9);
 	return (0);
 }
