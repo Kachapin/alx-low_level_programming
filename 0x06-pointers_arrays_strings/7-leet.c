778a90e49e2e6aaddec985469eea06e1df5fd9e5
Kachapin <kevinkachapin@gmail.com>
2022-09-23 01:15:04 +0300

    Write a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,46 +1,31 @@
-#include "main.h"
-
 /**
- * leet - function that encodes a string into
- * 1337
- * Letters a and A be replaced by 4
- * Letters e and E be replaced by 3
- * Letters o and O be replaced by 0
- * Letters t and T be replaced by 7
- * Letters l and L be replaced by 1
- * @s: character string
+ * reverse_array - a function that reverses the
+ *                content of an array of integers
+ *
+ * @a: pointer to int array
+ * @n: is the number of elements to swap
+ *
  * Return: nothing
- */
-char *leet(char *s)
-{
-	int i;
+*/
 
-	for (i = 0; s[i] != '\0'; i++)
+void reverse_array(int *a, int n)
+{
+	int temp, s, e;
+
+	s = 0;
+	e = n - 1;
+	/**
+	 * set value in array a in temp
+	 * then place the last array in
+	 * the first array then place
+	 * value in temp to last array
+	*/
+	while (s < e)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-		{
-			s[i] = '4';
-		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
-		}
-		else
-		{
-			continue;
-		}
+		temp = a[s];
+		a[s] = a[e];
+		a[e] = temp;
+		s++;
+		e--;
 	}
-	return (s);
 }
