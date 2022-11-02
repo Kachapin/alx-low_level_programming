f62edb0897e12d4b592d9be5a5c4a2394e91e448
Kachapin <kevinkachapin@gmail.com>
2022-11-03 01:41:36 +0300

    Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: main.h
- * Auth: Kelvin Kachapin
+ * Auth: Kelvin Lomerlim
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */
